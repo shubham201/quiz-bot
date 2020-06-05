b0c2bcd475df818d31adc7124f14cad564a2aaef
shubhambharti201 <shubhambharti201@gmail.com>
2020-06-06 04:34:27 +0530

    fix

--- main.js
+++ main.js
@@ -27,30 +27,30 @@ app.post("/start_bot", async function (req, res) {
         var chatId = message.chat.id;
         console.log(chatId);
         lastMessageId = currentMessageId;
-    }
-    if (message && message.text == undefined) {
-        if (message.poll) {
-            app.locals.poll = message.poll;
-            app.locals.IsActive = await tc.ask_options(telegram_url, chatId, res);
+        if (message.text == undefined) {
+            if (message.poll) {
+                app.locals.poll = message.poll;
+                app.locals.IsActive = await tc.ask_options(telegram_url, chatId, res);
+                res.end();
+            }
+            else {
+                res.end();
+            }
+        }
+        else if (app.locals.IsActive) {
+            await tc.sendPoll(telegram_url, chatId, app.locals.poll, message.text, res);
+            app.locals.poll = undefined; app.locals.IsActive = false;
             res.end();
         }
-        else {
-            res.end();
+        else if (message.text.toLowerCase().substr(0, 13).startsWith('/timeout_test')) {
+            await tc.timeout_test(message, res);
         }
-    }
-    else if (app.locals.IsActive) {
-        await tc.sendPoll(telegram_url, chatId, app.locals.poll, message.text, res);
-        app.locals.poll = undefined; app.locals.IsActive = false;
-        res.end();
-    }
-    else if (message.text.toLowerCase().substr(0, 13).startsWith('/timeout_test')) {
-        await tc.timeout_test(message, res);
-    }
-    else if (message.text.toLowerCase().startsWith('/start')) {
-        await tc.start(telegram_url, chatId, config.WELCOME, res);
-    }
-    else if (message.text.toLowerCase().substr(0, 1) == '/') {
-        await tc.fallback(telegram_url, chatId, res);
+        else if (message.text.toLowerCase().startsWith('/start')) {
+            await tc.start(telegram_url, chatId, config.WELCOME, res);
+        }
+        else if (message.text.toLowerCase().substr(0, 1) == '/') {
+            await tc.fallback(telegram_url, chatId, res);
+        }
     }
     else {
         res.end();

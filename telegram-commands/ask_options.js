ee3985017440cb824329422db4299a5151985f5d
Shubham Bharti <shubhambharti201@gmail.com>
2020-09-28 22:14:58 +0530

    5 options possible

--- telegram-commands/ask_options.js
+++ telegram-commands/ask_options.js
@@ -6,7 +6,7 @@ module.exports = function (telegram_url, senderId, res) {
             chat_id: senderId,
             text: "Answer not available for me. Choose the correct option :",
             reply_markup : {
-                keyboard : [[{"text" : "1"}, {"text" : "2"}], [{"text" : "3"}, {"text" : "4"}]]
+                keyboard : [[{"text" : "1"}, {"text" : "2"}, {"text" : "3"}], [{"text" : "4"}, {"text" : "5"}]]
             }
         }).then(resp => {
             res.send();
